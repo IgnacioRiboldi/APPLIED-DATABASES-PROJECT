12acb76b5b4bfb289a7da34d3cc65a426d78a000
Ignacio Riboldi <riboldi.ignacio@gmail.com>
2026-04-22 21:56:00 +0100

    2nd might be ready

--- Database.py
+++ Database.py
@@ -1,17 +1,19 @@
 import pymysql
 
 def connect():
-    return pymysq.connect(
-    host= "localhost", 
-    user="root", 
-    password="root", 
-    db="appdbproj")
+    return pymysql.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        db="appdbproj"
+    )
 
 def view_speakers_sessions():
     speaker = input("Enter speaker name: ")
-    
-    print (f"Sessions details for {speaker}")
-    print ("------------------------------------------")
+
+    print(f"\nSessions details for {speaker}")
+    print("------------------------------------------")
+
     conn = connect()
     cur = conn.cursor()
 
@@ -32,51 +34,73 @@ def view_speakers_sessions():
         print("No speaker was found of that name")
 
     conn.close()
-    
+
 def view_attendees_by_company():
-    if (not conn):
-        connect();
-        
-        query = """ SELECT * FROM teacher where experience < %s"""
-        
-    with conn:
-        
-       
-    
-    conn.close()
-    
-def add_new_attendee():
-    if (not conn):
-        connect();
-        
-        query = """ SELECT * FROM teacher where experience < %s"""
-        
-    with conn:
-        
-       
-    
-    conn.close()
-    
-def add_attendee_connection():
-    if (not conn):
-        connect();
-        
-        query = """ SELECT * FROM teacher where experience < %s"""
-        
-    with conn:
-        
-       
-    
+
+    conn = connect()
+    cur = conn.cursor()
+
+    while True:
+        user_input = input("Enter Company ID: ")
+        try:
+            company_id = int(user_input)
+            if company_id > 0:
+                break
+        except ValueError:
+            pass
+
+    cur.execute(
+        "SELECT companyName FROM company WHERE companyID = %s",
+        (company_id,)
+    )
+    company = cur.fetchone()
+
+    if not company:
+        print(f"Company with ID {company_id} doesn't exist")
+        conn.close()
+        return
+
+    company_name = company[0]
+
+    query = """
+        SELECT DISTINCT 
+            a.attendeeName, 
+            a.attendeeDOB, 
+            s.sessionTitle, 
+            s.speakerName, 
+            s.sessionDate, 
+            ro.roomName
+        FROM attendee a
+        INNER JOIN registration r 
+            ON a.attendeeID = r.attendeeID
+        INNER JOIN session s 
+            ON r.sessionID = s.sessionID
+        INNER JOIN room ro 
+            ON s.roomID = ro.roomID
+        WHERE a.attendeeCompanyID = %s
+    """
+
+    cur.execute(query, (company_id,))
+    rows = cur.fetchall()
+
+    if rows:
+        print(f"\n{company_name} Attendees:\n")
+        for attendeeName, attendeeDOB, sessionTitle, speakerName, sessionDate, roomName in rows:
+            print(f"Attendee: {attendeeName} | DOB: {attendeeDOB} | Session: {sessionTitle} | Speaker: {speakerName} | Date: {sessionDate} | Room: {roomName}")
+    else:
+        print(f"No attendees found for {company_name}")
+
     conn.close()
 
+
+
+def add_new_attendee():
+    print("Not implemented yet")
+
+
+def add_attendee_connection():
+    print("Not implemented yet")
+
+
 def view_rooms():
-    if (not conn):
-        connect();
-        
-        query = """ SELECT * FROM teacher where experience < %s"""
-        
-    with conn:
-        
-       
-    
-    conn.close()
+    print("Not implemented yet")
--- README.md
+++ README.md
@@ -14,3 +14,19 @@ https://www.w3schools.com/php/php_mysql_connect.asp
 Cursor:
 
 https://www.geeksforgeeks.org/sql/what-is-cursor-in-sql/
+
+Cur fetchall
+
+Return all (remaining) rows of a query result as a list. Return an empty list if no rows are available. Note that the arraysize attribute can affect the performance of this operation.
+
+https://docs.python.org/3/library/sqlite3.html#sqlite3.Cursor.fetchone
+
+Inner joins
+
+https://www.w3schools.com/sql/sql_join_inner.asp
+
+https://www.postgresql.org/docs/current/queries-table-expressions.html
+
+Input Validation:
+
+https://www.geeksforgeeks.org/python/input-validation-in-python/
